d8c12541d9ad10dbddbef7cc484a05733d7c7067
XiaTerry <1363314380@qq.com>
2018-07-28 23:02:05 +0800

    简单菜单导航和侧面菜单导航。

--- javascript/tab-Control.js
+++ javascript/tab-Control.js
@@ -3,14 +3,13 @@ var oCon = document.getElementById('content');
 var aLi = oUl.getElementsByTagName('li');
 var aDiv = oCon.getElementsByTagName('div');
 for(var i=0;i<aLi.length;i++){
-    debugger;
     aLi[i].index=i;
     aLi[i].onclick = function(){
         for(var i=0;i<aLi.length;i++){
             aLi[i].className='';
             aDiv[i].className='';
         }
-        // this.className = 'selected';
+        this.className = 'selected';
         aDiv[this.index].className='selected';
     };
 }
