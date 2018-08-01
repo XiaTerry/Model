4692c32c0ad433da0ce2a05d625a42ceaac99e86
XiaTerry <1363314380@qq.com>
2018-08-01 23:28:32 +0800

    星巴克主页之移动布局。

---
+++ starbucks/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+    var w = $('#content .banner img').width();
+    var nowIndex=1;
+    var timer;
+    var speed = 1500;
+    // console.log(w);
+    $('.header_li_5').on('click',function () {
+        // console.log(123);
+        $('.overlay').addClass('active');
+    });
+    $('.close').on('click',function () {
+        $('.overlay').removeClass('active');
+        return false;
+    });
+    $('#bnt').on('click',function () {
+        if(nowIndex == 4){
+            nowIndex=1;
+            $('#content .banner').css({
+                transition:'none',
+                transform:'translateX(0)'
+            })
+        }else{
+            $('#content .banner').css({
+                transition:'transform .8s ease',
+                transform:'translateX(-'+w*(nowIndex)+'px)'
+            });
+            nowIndex++;
+        }
+    });
+    $('.slide').on('mouseenter',function () {
+        clearInterval(timer);
+    });
+    $('.slide').on('mouseleave',function () {
+        play();
+    });
+    var play = function () {
+        if (nowIndex == 4) {
+            nowIndex = 1;
+            $('#content .banner').css({
+                transition: 'none',
+                transform: 'translateX(0)'
+            });
+            speed = 0;
+        } else {
+            $('#content .banner').css({
+                transition: 'transform .8s ease',
+                transform: 'translateX(-' + w * (nowIndex) + 'px)'
+            });
+            nowIndex++;
+            speed =1500;
+        }
+        clearInterval(timer);
+        timer = setInterval(play,speed)
+    };
+    timer = setInterval(play,speed);
+});
